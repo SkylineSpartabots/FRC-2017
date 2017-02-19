15e1e9e7f6900b9c4b3fa10a8088c0621585aadc
Aayushi Modi <modi.aayu@gmail.com>
2017-02-18 23:29:45 -0800

    Added ultrasonic code to RPS

--- src/util/RPS.java
+++ src/util/RPS.java
@@ -1,17 +1,21 @@
 package util;
+
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The RPS (Robot Positioning system) attempts to give the most accurate information about the current heading of the robot
+ * The RPS (Robot Positioning system) attempts to give the most accurate
+ * information about the current heading of the robot
  * 
- * This class will run three threads that will simultaneously calculate, integrate and filter data from the following sensors:
+ * This class will run three threads that will simultaneously calculate,
+ * integrate and filter data from the following sensors:
  * 
- * Sensors: Wheel Encoders on Mecanum drive (this will be more complicated than normal)
- * ADNS Laser Tracker: a mouse
- * NAVX: experimental data double integration of acceleration
+ * Sensors: Wheel Encoders on Mecanum drive (this will be more complicated than
+ * normal) ADNS Laser Tracker: a mouse NAVX: experimental data double
+ * integration of acceleration
  * 
  * @author NeilHazra
  */
@@ -19,26 +23,35 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class RPS {
 	public AHRS ahrs;
 	public ADNS_I2C adns_I2C;
-	
-	
+
+	private AnalogInput sonarInput;
 	
 	public RPS() {
-		ahrs = new AHRS(SPI.Port.kMXP);	
+		ahrs = new AHRS(SPI.Port.kMXP);
 		adns_I2C = new ADNS_I2C();
-	}	
-	
-	
-	public double getXDisplacementADNS()	{
+		sonarInput = new AnalogInput(0); // ultrasonic is connected to port 0
+	}
+
+	public double getXDisplacementADNS() {
 		return adns_I2C.getX();
 	}
-	public double getYDisplacementADNS()	{
+
+	public double getYDisplacementADNS() {
 		return adns_I2C.getY();
 	}
-	public double getHeadingADNS()	{
-		return adns_I2C.getTheta(); 
+
+	public double getHeadingADNS() {
+		return adns_I2C.getTheta();
 	}
-	public double getAngle()	{
+
+	public double getAngle() {
 		return ahrs.getAngle();
 	}
-	
+
+	public double getDistance() {
+		double distance = ((sonarInput.getAverageVoltage() * 512) / 5);
+		SmartDashboard.putNumber("distance", distance);
+		
+		return distance;
+	}
 }
